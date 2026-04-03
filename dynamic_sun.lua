481dc6e48c20b97d0b45484edac7cc6144ac2911
Java <java@proxeninc.net>
2026-04-03 15:30:35 -0400

    Refactor dynamic sun script configuration and logic
    Refactor configuration settings and improve time handling.

--- dynamic_sun.lua
+++ dynamic_sun.lua
@@ -1,197 +1,170 @@
+--- Dynamic Sun Bigass V3 Script ---
+---	Originally by Altis94 ---
+--- Updated by Java ---
 
 api_version = "1.12.0.0"
 
---CONFIG
-	sky_id = 4
-	
-	sun_yaw = 0
-	sun_pitch = 90
-	sun_move_ratio = 31
-	
-	use_real_time = false
-	time_speed = 0.0003 -- how fast time changes
-	time = 11 -- starting time in hours
-	
-	-- hour, rgb, fog distance, fog density, tint type, tint intensity
-	COLORS_OF_SKY = {
-		[0] = {15,14,20, 		5, 0.75,	6, 0},--midnight
-		[1] = {14,14,22, 		10, 0.75,	6, 0},
-		[2] = {14,14,25, 		20, 0.76,	6, 0},
-		[3] = {25,26,36, 		40, 0.77,	6, 0},
-		[4] = {45,46,61, 		60, 0.78,	6, 0},
-		[5] = {63,64,87, 		70, 0.79,	6, 0},
-		[6] = {184,143,127, 	70, 0.76,	1, 0.00001},--sunrise
-		[7] = {213,187,170,		70, 0.7,	1, 0.0027},
-		[8] = {170,182,194, 	70, 0.68,	1, 0.0023},
-		[9] = {126,150,173, 	70, 0.66,	1, 0.002},
-		[10] = {132,159,185,	40, 0.63,	1, 0.0015},
-		[11] = {120,170,192,	70, 0.6,	1, 0.0006},
-		[12] = {133,179,198, 	80, 0.57,	1, 0.0003},
-		[13] = {133,179,198, 	90, 0.54,	1, 0.0001},
-		[14] = {121,157,181, 	100, 0.52,	1, 0.0001},
-		[15] = {116,143,169, 	100, 0.52,	1, 0.0001},
-		[16] = {103,126,144, 	80, 0.54,	1, 0.004},
-		[17] = {94,111,98, 		70, 0.56,	1, 0.005},
-		[18] = {145,140,86, 	50, 0.6,	1, 0.01},
-		[19] = {156,123,69, 	40, 0.65,	1, 0.018},
-		[20] = {123,58,23, 		30, 0.7,	1, 0.035},
-		[21] = {105,53,25, 		20, 0.75,	1, 0.025},
-		[22] = {66,34,20, 		15, 0.8,	1, 0.00001},
-		[23] = {48,27,16, 		10, 0.78,	6, 0},
-		[24] = {15,14,20, 		5, 0.75,	6, 0},-- must be same as 0
-	}
-	
-	message_update_rate = 10 -- how many ticks to skip before sending the clients data
-	message_update_rate_colors = 53
-	
-	show_time = false
-	announce_hours = true
+-- CONFIG
+local sky_id = 4
+local sun_yaw = 0
+local sun_move_ratio = 30
+
+local use_real_time = false
+local time_speed = 0.0002 
+local time = 11 
+
+-- hour = { r, g, b, fog_dist, fog_dens, tint_type, tint_intensity }
+local COLORS_OF_SKY = {
+    [0]  = {10, 10, 18,     5,   0.78, 6, 0},
+    [1]  = {12, 12, 20,     8,   0.77, 6, 0},
+    [2]  = {14, 14, 24,     15,  0.76, 6, 0},
+    [3]  = {20, 22, 32,     30,  0.75, 6, 0},
+    [4]  = {35, 38, 52,     45,  0.74, 6, 0},
+    [5]  = {60, 62, 85,     60,  0.73, 6, 0},
+    [6]  = {170, 120, 105,  70,  0.70, 1, 0.0005},
+    [7]  = {205, 165, 150,  75,  0.68, 1, 0.002},
+    [8]  = {185, 190, 205,  80,  0.66, 1, 0.002},
+    [9]  = {150, 170, 190,  85,  0.64, 1, 0.0018},
+    [10] = {135, 175, 205,  90,  0.60, 1, 0.0012},
+    [11] = {120, 185, 210,  95,  0.58, 1, 0.0008},
+    [12] = {115, 195, 220,  100, 0.55, 1, 0.0005},
+    [13] = {115, 195, 220,  100, 0.53, 1, 0.0004},
+    [14] = {110, 175, 205,  100, 0.52, 1, 0.0004},
+    [15] = {105, 160, 185,  95,  0.52, 1, 0.0004},
+    [16] = {120, 140, 150,  80,  0.54, 1, 0.003},
+    [17] = {140, 130, 110,  70,  0.56, 1, 0.004},
+    [18] = {165, 140, 90,   60,  0.60, 1, 0.008},
+    [19] = {180, 120, 70,   50,  0.65, 1, 0.015},
+    [20] = {150, 70,  35,   40,  0.70, 1, 0.030},
+    [21] = {110, 55,  28,   25,  0.75, 1, 0.020},
+    [22] = {70,  35,  22,   15,  0.78, 1, 0.002},
+    [23] = {40,  25,  15,   10,  0.78, 6, 0},
+    [24] = {10, 10, 18,     5,   0.78, 6, 0},
+}
+
+local message_update_rate = 15 
+local message_update_rate_colors = 30
+local show_time = false
+local announce_hours = true
 --END OF CONFIG
 
-local sin = math.sin
-local cos = math.cos
-local rad = math.rad
-local console_timer = 10
-local console_timer2 = -5
+local timer1, timer2 = 0, 0
 local previous_hour = -1
+local current_tod = -1
 
 function OnScriptLoad()
-	register_callback(cb['EVENT_COMMAND'],"OnCommand")
-	register_callback(cb["EVENT_TICK"],"OnTick")
-	for id,rgb in pairs (COLORS_OF_SKY) do
-		for i=1,3 do
-			COLORS_OF_SKY[id][i] = COLORS_OF_SKY[id][i] / 255
-		end
-	end
+    register_callback(cb['EVENT_COMMAND'], "OnCommand")
+    register_callback(cb["EVENT_TICK"], "OnTick")
+    register_callback(cb["EVENT_GAME_START"], "OnGameStart")
+
+    -- Normalize RGB values once on load
+    for h, data in pairs(COLORS_OF_SKY) do
+        data[1], data[2], data[3] = data[1]/255, data[2]/255, data[3]/255
+    end
 end
 
-function OnScriptUnload()
+-- Helper for smooth transitions
+local function lerp(a, b, t) return a + (b - a) * t end
+
+local function UpdateTimeAndSun()
+    if use_real_time then
+        local h = tonumber(os.date("%H"))
+        local m = tonumber(os.date("%M"))
+        local s = tonumber(os.date("%S"))
+        time = h + (m/60) + (s/3600)
+    else
+        time = (time + time_speed) % 24
+    end
+
+    local temp_time = (time - 6) % 24
+    local sun_pitch = (temp_time / sun_move_ratio) * 360
+    return math.floor(time), time % 1, sun_pitch
+end
+
+local function SetTOD(t)
+    local target = (t >= 8 and t < 20) and 0 or 1
+    if current_tod ~= target then
+        execute_command_sequence("set tod " .. target)
+        current_tod = target
+    end
+end
+
+function OnGameStart()
+    current_tod = -1 -- Reset state
+    SetTOD(time)
 end
 
 function OnTick()
-	local min
-	local hours
-	local minutes
-	local temp_time
-	
-	if show_time then
-		ClearConsole()
-	end
-	if use_real_time == false then
-		time = time + time_speed
-		if time > 24 then
-			time = 0
-		end
-		
-		temp_time = time - 6
-		if temp_time < 0 then
-			temp_time = temp_time + 24
-		end
-		min = time%1
-		hours = math.floor(time)
-		minutes = math.floor((min)*60)
-	else
-		min = os.date("%M")/60
-		hours = os.date("%H")
-		minutes = os.date("%M")
-		seconds = os.date("%S")
-		temp_time = hours + min - 6
-	end
-	
-	sun_pitch = temp_time/sun_move_ratio * 360
-	
-	if show_time then
-		rprint(1, "time "..string.format("%02d",hours)..":"..string.format("%02d",minutes))
-	end
-	--rprint(1, "pitch "..math.floor(sun_pitch))
-	
-	if announce_hours and previous_hour ~= hours then
-		if time > 12 then
-			if time > 13 then
-				say_all("It's "..hours - 12 .."PM...")
-			else
-				say_all("It's 12PM...")
-			end
-		else
-			if time > 1 then
-				say_all("It's "..hours .."AM...")
-			else
-				say_all("It's 12AM...")
-			end
-		end
-	end
-	previous_hour = hours
-	
-	local r1,r2 = COLORS_OF_SKY[hours][1], COLORS_OF_SKY[hours+1][1]
-	local g1,g2 = COLORS_OF_SKY[hours][2], COLORS_OF_SKY[hours+1][2]
-	local b1,b2 = COLORS_OF_SKY[hours][3], COLORS_OF_SKY[hours+1][3]
-	local red = r1 * (1 - min) + r2 * min
-	local green = g1 * (1 - min) + g2 * min
-	local blue = b1 * (1 - min) + b2 * min
-	
-	local t1,t2 = COLORS_OF_SKY[hours][7], COLORS_OF_SKY[hours+1][7]
-	local tint = t1 * (1 - min) + t2 * min
-	local tint_type = COLORS_OF_SKY[hours][6]
-	
-	console_timer = console_timer + 1
-	console_timer2 = console_timer2 + 1
-	if console_timer > message_update_rate then
-		console_timer = 0
-		for i=1,16 do
-			if player_present(i) and get_var(i, "$has_chimera") == "1" then
-				rprint(i, "fsky~"..sky_id.."~"..rad(sun_yaw).."~"..rad(sun_pitch))
-			end
-		end
-	end
-	if console_timer2 > message_update_rate_colors then
-		console_timer2 = 0
-		for i=1,16 do
-			if player_present(i) and get_var(i, "$has_chimera") == "1" then
-				rprint(i, "fscreen_tint~"..tint_type.."~"..round(tint, 5).."~"..round(red, 4).."~"..round(green, 4).."~"..round(blue, 4))
-				rprint(i, "ffog~"..round(red, 3).."~"..round(green, 3).."~"..round(blue, 3).."~"..COLORS_OF_SKY[hours][5].."~0~"..COLORS_OF_SKY[hours][4].."~0~0")
-			end
-		end
-	end
+    local hour, fraction, sun_pitch = UpdateTimeAndSun()
+    SetTOD(time)
+
+    if show_time then ClearConsole() end
+
+    -- Announce hours (12-hour format logic)
+    if announce_hours and previous_hour ~= hour then
+        local display_h = hour % 12
+        if display_h == 0 then display_h = 12 end
+        local suffix = hour < 12 and "AM" or "PM"
+        say_all(string.format("It's %d%s...", display_h, suffix))
+        previous_hour = hour
+    end
+
+    -- Interpolate sky values
+    local c1, c2 = COLORS_OF_SKY[hour], COLORS_OF_SKY[hour + 1]
+    local r = lerp(c1[1], c2[1], fraction)
+    local g = lerp(c1[2], c2[2], fraction)
+    local b = lerp(c1[3], c2[3], fraction)
+    local f_dist = lerp(c1[4], c2[4], fraction)
+    local f_dens = lerp(c1[5], c2[5], fraction)
+    local tint_i = lerp(c1[7], c2[7], fraction)
+    local tint_t = c1[6]
+
+    timer1 = timer1 + 1
+    timer2 = timer2 + 1
+
+    -- Combined Player Loop for efficiency
+    if timer1 > message_update_rate or timer2 > message_update_rate_colors then
+        for i = 1, 16 do
+            if player_present(i) and get_var(i, "$has_chimera") == "1" then
+                if timer1 > message_update_rate then
+                    rprint(i, string.format("fsky~%d~%.4f~%.4f", sky_id, math.rad(sun_yaw), math.rad(sun_pitch)))
+                end
+                if timer2 > message_update_rate_colors then
+                    rprint(i, string.format("fscreen_tint~%d~%.5f~%.4f~%.4f~%.4f", tint_t, tint_i, r, g, b))
+                    rprint(i, string.format("ffog~%.3f~%.3f~%.3f~%.2f~0~%.1f~0~0", r, g, b, f_dens, f_dist))
+                end
+            end
+        end
+        if timer1 > message_update_rate then timer1 = 0 end
+        if timer2 > message_update_rate_colors then timer2 = 0 end
+    end
 end
 
 function ClearConsole()
-	for i=1,16 do
-		if player_present(i) then
-			for j=0,27 do
-				rprint(i, " ")
-			end
-		end
-	end
+    for i = 1, 16 do
+        if player_present(i) then
+            for j = 0, 25 do rprint(i, " ") end
+        end
+    end
 end
 
-function OnCommand(i,Command,Environment,Password)
-	MESSAGE = {}
-	for word in string.gmatch(Command, "([^".." ".."]+)") do 
-		table.insert(MESSAGE, word)
-	end
-	
-	if MESSAGE[1] == "set_time" then
-		if get_var(i, "$lvl") > "2" or Environment == 0 then
-			local wanted_time = MESSAGE[2]
-			if wanted_time ~= nil and tonumber(wanted_time) ~= nil then
-				wanted_time = tonumber(wanted_time)
-				if wanted_time >= 0 and wanted_time <= 24 then
-					time = wanted_time
-				else
-					say(i, "time must be between 0 and 24 hours!")
-				end
-			else
-				say(i, "set_time <time>")
-			end
-		else
-			say(i, "you don't have permission to use this command")
-		end
-		return false
-	end
-	return true
-end
+function OnCommand(i, command, env, password)
+    local args = {}
+    for word in command:gmatch("%S+") do table.insert(args, word) end
 
-function round(num, numDecimalPlaces)
-  local mult = 10^(numDecimalPlaces or 0)
-  return math.floor(num * mult + 0.5) / mult
+    if args[1] == "set_time" then
+        if tonumber(get_var(i, "$lvl")) > 2 or env == 0 then
+            local val = tonumber(args[2])
+            if val and val >= 0 and val <= 24 then
+                time = val
+                previous_hour = -1 -- Trigger re-announcement
+            else
+                say(i, "Usage: set_time <0-24>")
+            end
+        else
+            say(i, "No permission.")
+        end
+        return false
+    end
+    return true
 end
